34fa0c8253327ec5a0038f4bde548cb991e1fcb9
Erick Kurniawan <erick.kurniawan@gmail.com>
2026-09-17 18:14:58 +0700

    Add project files.

---
+++ BlazingPizzaApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazingPizzaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "BlazingPizzaApp" };
+        }
+    }
+}
---
+++ BlazingPizzaApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazingPizzaApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
