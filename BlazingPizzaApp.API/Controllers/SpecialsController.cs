52e10b952a2c1471d61bbb830504b5cfec65973e
Erick Kurniawan <erick.kurniawan@gmail.com>
2026-09-24 18:23:30 +0700

    menambahkan controller

---
+++ BlazingPizzaApp.API/Controllers/SpecialsController.cs
@@ -0,0 +1,25 @@
+﻿using BlazingPizzaApp.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazingPizzaApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialsController : ControllerBase
+    {
+        private readonly IPizzaSpecials _pizzaSpecials;
+
+        public SpecialsController(IPizzaSpecials pizzaSpecials)
+        {
+            _pizzaSpecials = pizzaSpecials;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var specials = await _pizzaSpecials.GetPizzaSpecialsAsync();
+            return Ok(specials);
+        }
+    }
+}
